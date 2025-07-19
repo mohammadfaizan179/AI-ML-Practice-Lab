acdb8a16ce2ea265392d46ca77e0150b66e37982
Muhammad Faizan <faizan.meo179@gmail.com>
2025-07-19 20:47:14 +0500

    chore: work on mine vs rock prediction project

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ from NumPy.main import practice_numpy
 from Pandas.main_pandas import practice_pandas
 
 if __name__ == '__main__':
-    # practice_numpy()
+    practice_numpy()
     practice_pandas()
 
